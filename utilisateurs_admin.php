3f592c2fcbd58320b557348ddefd65eb22b206c3
RayanAdmin <50583793+RayanAdmin@users.noreply.github.com>
2020-03-30 00:15:27 +0200

    Add files via upload

---
+++ utilisateurs_admin.php
@@ -0,0 +1,184 @@
+<?php
+
+//Permet de garder les variables de la session
+session_start();
+//Connexion à notre base de donnée
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_membre;charset=utf8', 'root', '');
+//Permet de récupere les informations des utilisateurs
+$membres = $bdd->query('SELECT * FROM membres ORDER BY id DESC LIMIT 0,5');
+
+//Restrindre l'accés à cette page au personne non connecté
+ if(!isset($_SESSION['id'])) {
+
+         header('Location: errorConnexion.html');
+         exit;
+
+   }
+//Restrindre l'accés à cette page au personne qui ne sont pas administrateur
+ if (strcasecmp($_SESSION['droit'], 'admin') ==! 0){
+
+         header('Location: errorAdmin.html');
+         exit;
+      
+   }
+   	
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Profil des utilisateurs du site</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/contact.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="assets/css/navigation.css">
+    <link rel="stylesheet" href="assets/css/profil.css">
+
+
+</head>
+<body>
+<nav class="navbar navbar-light navbar-expand-md shadow-lg navigation-clean-button" style="background-color: #313437;">
+    <div class="container"><a class="navbar-brand" href="index.php" style="color: #ffffff;">SmartDoc</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="nav navbar-nav mr-auto">
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"  style="color: white !important;">
+                        Mon profil
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="profil.php">Afficher mon profil</a>
+                        <a class="dropdown-item" href="editionprofil.php">Editer mon profil</a>
+                        <a class="dropdown-item" href="supp_account.php">Supprimer mon compte</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"  style="color: white !important;">
+                        Documents
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="document.php">Afficher les documents publiques</a>
+                        <a class="dropdown-item" href="mydocument.php">Afficher mes documents</a>
+                        <a class="dropdown-item" href="upload.php">Upload un document</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"  style="color: white !important;">
+                        Statistiques
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="stat_extension.php">Par extension</a>
+                        <a class="dropdown-item" href="stat_public.php">Publiques / Privés</a>
+                    </div>
+                </li>
+
+
+                <?php
+                 //Rajout de la barre d'administration si la personne est un administrateur
+                if (strcasecmp($_SESSION['droit'], 'admin') == 0){
+
+
+                    echo '<li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"  style="color: white !important;">
+                        Administration
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="utilisateurs_admin.php">Afficher tous les utilisateurs</a>
+                        <a class="dropdown-item" href="affich_docs.php">Afficher les documents des utilisateurs</a>
+                        <a class="dropdown-item" href="modif_utlisateurs_admin.php">Modifier / Supprimer un utilisateur</a>
+                        <a class="dropdown-item" href="create_utilisateurs.php">Créer un utilisateur</a>
+                        <a class="dropdown-item" href="stat_admin.php">Statistiques des utilisateurs</a>
+                    </div>
+                </li>';
+
+
+                }
+                ?>
+
+
+            </ul><span class="navbar-text actions"> <a class="btn btn-light action-button" role="button" href="deconnexion.php">Déconnexion</a></span>
+
+        </div>
+    </div>
+</nav>
+
+<div align="center">
+<div class="container">
+
+  <h1 style="margin-top: 50px">Liste des utilisateurs</h1>
+    <i class="fa fa-users fa-5x"></i></br>
+
+  	</br>
+  	</br>
+    <table id="example" class="table table-striped table-bordered" style="width:100%">
+        <thead>
+        <tr>
+       <th>Id</th>
+       <th>Pseudo</th>
+       <th>Mail</th>
+       <th>Droit</th>
+   </tr>
+        </thead>
+
+        <tbody>
+   
+      <?php while($m = $membres->fetch()) { ?>
+	      <tr>
+	      	<th><?= $m['id'] ?> </th>
+	      	<th><?= $m['pseudo'] ?></th>
+	      	<th><?= $m['mail'] ?></th>
+          <th><?= $m['droit'] ?></th>
+	       </tr>
+
+      <?php
+  	  }
+  	  ?>
+        </tbody>
+  	</table>
+  	</br>
+  	</br>
+
+
+</div>
+</div>
+
+<div class="footer">
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 item text">
+                    <h3>SmartDoc</h3>
+                    <p>Jeune start-up, SmartDoc souhaite développer sa vison du stockage de document, accessible partout et sur tout support</p>
+                </div>
+
+            </div>
+            <p class="copyright">SmartDoc © 2019</p>
+        </div>
+    </footer>
+</div>
+
+</body>
+</html>
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/js/chart.min.js"></script>
+<script src="assets/js/bs-animation.js"></script>
+<script src="assets/js/bs-charts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
+<script src="assets/js/jquery-3.3.1.js"></script>
+<script src="assets/js/jquery.dataTables.min.js"></script>
+<script src="assets/js/dataTables.bootstrap4.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#example').DataTable();
+    } );
+
+</script>
